8fcf5c13bd9011b55fd316e0807d381988ed0769
sahidcse15@gmail.com <sahidcse15@gmail.com>
2023-01-02 11:44:56 +0530

    Initial Commit

---
+++ Dice-SwiftUI/Dice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_SwiftUIApp.swift
+//  Dice-SwiftUI
+//
+//  Created by Sahid Reza on 02/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Dice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
